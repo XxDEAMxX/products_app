4e5ae04803eea004cc353974169f7d90b2c08c59
XxDEAMxX <deam10hw@gmail.com>
2023-07-10 19:44:52 -0500

    Formulario de Login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -21,14 +21,57 @@ class LoginScreen extends StatelessWidget {
                     Text('Login', style: Theme.of(context).textTheme.headline4,),
                     SizedBox(height: 30,),
 
-                    Text('Formulario')
+                    _LoginForm()
                   ],
                 )
-              )
+              ),
+              SizedBox(height: 50,),
+              Text('Crear una nueva cuenta', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
             ],
           ),
         )
       )
+    );
+  }
+}
+
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        //TODO: mantener la referencia al KEY
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.purple,
+                    width: 2
+                  )
+                ),
+                hintText: 'Email',
+                labelText: 'Correo electronico',
+                labelStyle: TextStyle(
+                  color: Colors.grey
+                ),
+                prefixIcon: Icon(Icons.alternate_email_sharp, color: Colors.deepPurple,)
+              ),
+            ),
+          ]
+        ),
+      ),
     );
   }
 }
