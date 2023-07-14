eec88fc6c3938807610198772641b1271845520d
XxDEAMxX <deam10hw@gmail.com>
2023-07-14 13:51:57 -0500

    Pantalla de producto - Parte 2

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -7,22 +7,113 @@ class ProductScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-
-
             Stack(
               children: [
-                ProductImage()
+                const ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(), 
+                    icon: const Icon(Icons.arrow_back_ios_new, size: 40, color: Colors.white,), 
+                  )
+                ),
+                Positioned(
+                  top: 60,
+                  right: 20,
+                  child: IconButton(
+                    onPressed: () {
+                      // TODO: Camara o galeria
+                    }, 
+                    icon: const Icon(Icons.photo_camera, size: 40, color: Colors.white,), 
+                  )
+                )
               ],
+            ),
 
+            _ProductForm(),
 
-            )
+            SizedBox(height: 100,)
           ],
         ),
-      )
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          //TODO: Guardar producto
+        },
+        child: Icon(Icons.save_outlined),
+      ),
     );
   }
+}
+
+class _ProductForm extends StatelessWidget {
+  const _ProductForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: _buildBoxDecoration(),
+        width: double.infinity,
+        child: Form(
+          child: Column(
+            children: [
+              const SizedBox(height: 10,),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Nombre del producto',
+                  labelText: 'Nombre',
+                  border: OutlineInputBorder()
+                ),
+              ),
+              const SizedBox(height: 30,),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  hintText: '\$150',
+                  labelText: 'Precio',
+                  border: OutlineInputBorder()
+                ),
+              ),
+              const SizedBox(height: 30,),
+
+              SwitchListTile.adaptive(
+                value: true, 
+                title: Text('Diponible'),
+                activeColor: Colors.indigo,
+                onChanged: (value) {
+                  
+                },
+              ),
+
+              const SizedBox(height: 30,)
+            ],
+          )
+        ),
+        
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+    color: Colors.white,
+    borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25)),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black.withOpacity(0.05),
+        offset: const Offset(0, 5),
+        blurRadius: 10
+      )
+    ]
+  );
 }
--- lib/widgets/product_image.dart
+++ lib/widgets/product_image.dart
@@ -11,9 +11,9 @@ class ProductImage extends StatelessWidget {
         decoration: _buildBoxDecoration(),
         width: double.infinity,
         height: 450,
-        child: ClipRRect(
+        child: const ClipRRect(
           borderRadius: BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
-          child: const FadeInImage(
+          child: FadeInImage(
             placeholder: AssetImage('assets/jar-loading.gif'), 
             image: NetworkImage('https://via.placeholder.com/400x300/green'),
             fit: BoxFit.cover,
@@ -25,7 +25,7 @@ class ProductImage extends StatelessWidget {
 
   BoxDecoration _buildBoxDecoration() {
     return BoxDecoration(
-      color: Colors.red,
+      //color: Colors.red,
       borderRadius: const BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
       boxShadow: [
         BoxShadow(
