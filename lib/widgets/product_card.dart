894d2f4945954451c880bd0a004340f27a0503ae
XxDEAMxX <deam10hw@gmail.com>
2023-07-13 20:12:55 -0500

    Widget - ProductCard - Parte 2

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -15,8 +15,22 @@ class ProductCard extends StatelessWidget {
         child: Stack(
           alignment: Alignment.bottomLeft,
           children: [
+
             _BackgroundImage(),
-            _productDetails()
+            
+            _productDetails(),
+
+            Positioned(
+              top: 0,
+              right: 0,
+              child: _PriceTitle(),
+            ),
+
+            Positioned(
+              top: 0,
+              left: 0,
+              child: _notAvailable(),
+            )
           ],
         ),
       ),
@@ -38,6 +52,56 @@ class ProductCard extends StatelessWidget {
   }
 }
 
+class _notAvailable extends StatelessWidget {
+  const _notAvailable({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text('No Disponible', style: TextStyle(color: Colors.white, fontSize: 20),),
+        ),
+      ),
+      width: 100,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(25), bottomRight: Radius.circular(25))
+      ),
+    );
+  }
+}
+
+class _PriceTitle extends StatelessWidget {
+  const _PriceTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text('\$103.99', style: TextStyle(color: Colors.white, fontSize: 20), )),
+      ),
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(topRight: Radius.circular(25), bottomLeft: Radius.circular(25))
+      ),
+    );
+  }
+}
+
 class _productDetails extends StatelessWidget {
   const _productDetails({
     super.key,
@@ -45,11 +109,38 @@ class _productDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 70,
-      color: Colors.red,
+    return Padding(
+      padding: EdgeInsets.only(right: 50),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 70,
+        
+        decoration: _DataBoxProduct(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Dico duro g', 
+              style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold,), 
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              'Id del disco duro', 
+              style: TextStyle(fontSize: 15, color: Colors.white,)
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  BoxDecoration _DataBoxProduct() {
+    return BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25), topRight: Radius.circular(25))
+      );
   }
 }
 
