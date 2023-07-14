eb5dc39ada37d210d09d049cf4aa4eba2ef8d287
XxDEAMxX <deam10hw@gmail.com>
2023-07-13 19:23:04 -0500

    Widget - ProductCard

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -11,8 +11,65 @@ class ProductCard extends StatelessWidget {
         margin: EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
         height: 400,
-        color: Colors.red,
+        decoration: _cardBorders(),
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            _BackgroundImage(),
+            _productDetails()
+          ],
+        ),
+      ),
+    );
+  }
 
+  BoxDecoration _cardBorders() {
+    return BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, 7),
+            blurRadius: 10
+          )
+        ]
+      );
+  }
+}
+
+class _productDetails extends StatelessWidget {
+  const _productDetails({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      color: Colors.red,
+    );
+  }
+}
+
+class _BackgroundImage extends StatelessWidget {
+  const _BackgroundImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+          fit: BoxFit.cover
+        ),
       ),
     );
   }
